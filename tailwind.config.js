7cd94de22cf6f239b15be9a0e1db5e72fc7c976a
shewood <shewood01@gmail.com>
2022-01-20 19:40:19 -0500

    start with layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        display: ['Lato', 'sans-serif'],
+        body: ['Noto sans', 'sans-serif'],
+      },
       colors: {
         current: 'currentColor',
+      },
+      backgroundImage: () => ({
+        'footer-parliament-image': 'url(../public/landscape.png)',
+        // 'splash-page': 'url(../public/sp-bg-1.jpg)',
+      }),
+      boxShadow: {
+        card: '0px 2px 8px rgba(0, 0, 0, 0.25)',
       },
     },
   },
