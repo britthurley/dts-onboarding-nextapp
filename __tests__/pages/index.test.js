e2725b5478ada2716ad4616c8ae6ef275733b228
MarcoGoC <marcogoc951@gmail.com>
2022-02-03 14:24:21 -0500

    testing getStaticProps on index page

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -4,12 +4,20 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Index from '../../pages/index'
+import { getStaticProps } from '../../pages/index'
 
 import { useRouter } from 'next/router'
 
 // mocks useRouter to be able to use component' router.asPath
 jest.mock('next/router', () => ({
   useRouter: jest.fn(),
+}))
+
+// 'Mock' call to fetchContent
+jest.mock('../../lib/cms', () => ({
+  fetchContent: () => {
+    return {}
+  },
 }))
 
 describe('index page', () => {
@@ -40,5 +48,29 @@ describe('index page', () => {
     render(<Index locale="en" meta={meta} />)
     const heading = screen.getByRole('heading')
     expect(heading).toBeInTheDocument()
+  })
+
+  it('Test getStaticProps', async () => {
+    const props = await getStaticProps({ locale: 'en' })
+
+    expect(props).toEqual({
+      props: {
+        locale: 'en',
+        meta: {
+          data_en: {
+            desc: 'English',
+            author: 'Service Canada',
+            keywords: '',
+            title: 'Next Template - Canada.ca',
+          },
+          data_fr: {
+            author: 'Service Canada',
+            desc: 'Français',
+            keywords: '',
+            title: 'Next Template - Canada.ca',
+          },
+        },
+      },
+    })
   })
 })
